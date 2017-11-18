7b107bae5199eda40e460950a45c0afb364b484c
ayvengoza <ayvengoza@gmail.com>
2017-11-19 00:26:57 +0200

    add sun animation

--- app/src/main/java/com/ayvengoza/sunset/SunsetFragment.java
+++ app/src/main/java/com/ayvengoza/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.ayvengoza.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,6 +13,9 @@ import android.view.ViewGroup;
  */
 
 public class SunsetFragment extends Fragment {
+    private View mSceveView;
+    private View mSunView;
+    private View mSkyView;
 
     public static SunsetFragment newInstance(){
         Bundle args = new Bundle();
@@ -24,6 +28,24 @@ public class SunsetFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        mSceveView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceveView.setOnClickListener(v -> startAnimation());
+
         return view;
+    }
+
+    private void startAnimation(){
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+
+        heightAnimator.start();
     }
 }
